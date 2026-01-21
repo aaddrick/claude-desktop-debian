4174c2099f334181e58a7c867aae590afcee7fb1
aaddrick <aaddrick@gmail.com>
2026-01-20 21:25:14 -0500

    fix: hide menu bar on Linux by intercepting setApplicationMenu
    The app calls Menu.setApplicationMenu() asynchronously after window
    creation, which causes the menu bar to appear even when we hide it
    in the BrowserWindow constructor.
    
    This fix intercepts Menu.setApplicationMenu() and automatically hides
    the menu bar on all windows after the menu is set, ensuring it stays
    hidden regardless of when the app sets the menu.
    
    Changes:
    - Add autoHideMenuBar: true to BrowserWindow options
    - Call setMenuBarVisibility(false) after window creation
    - Intercept Menu.setApplicationMenu to hide menu bar on all windows
    
    The menu bar can still be toggled with the Alt key if needed.
    
    Fixes #155

--- build.sh
+++ build.sh
@@ -415,6 +415,7 @@ Module.prototype.require = function(id) {
   if (id === 'electron') {
     console.log('[Frame Fix] Intercepting electron module');
     const OriginalBrowserWindow = module.BrowserWindow;
+    const OriginalMenu = module.Menu;
 
     module.BrowserWindow = class BrowserWindowWithFrame extends OriginalBrowserWindow {
       constructor(options) {
@@ -424,12 +425,19 @@ Module.prototype.require = function(id) {
           const originalFrame = options.frame;
           // Force native frame
           options.frame = true;
+          // Hide the menu bar by default (Alt key will toggle it)
+          options.autoHideMenuBar = true;
           // Remove custom titlebar options
           delete options.titleBarStyle;
           delete options.titleBarOverlay;
           console.log(`[Frame Fix] Modified frame from ${originalFrame} to true`);
         }
         super(options);
+        // Hide menu bar after window creation on Linux
+        if (process.platform === 'linux') {
+          this.setMenuBarVisibility(false);
+          console.log('[Frame Fix] Menu bar visibility set to false');
+        }
       }
     };
 
@@ -446,6 +454,21 @@ Module.prototype.require = function(id) {
         }
       }
     }
+
+    // Intercept Menu.setApplicationMenu to hide menu bar on Linux
+    // This catches the app's later calls to setApplicationMenu that would show the menu
+    const originalSetAppMenu = OriginalMenu.setApplicationMenu.bind(OriginalMenu);
+    module.Menu.setApplicationMenu = function(menu) {
+      console.log('[Frame Fix] Intercepting setApplicationMenu');
+      originalSetAppMenu(menu);
+      if (process.platform === 'linux') {
+        // Hide menu bar on all existing windows after menu is set
+        for (const win of module.BrowserWindow.getAllWindows()) {
+          win.setMenuBarVisibility(false);
+        }
+        console.log('[Frame Fix] Menu bar hidden on all windows');
+      }
+    };
   }
 
   return module;
